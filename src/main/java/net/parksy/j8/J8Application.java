0abdcf9e08dee6f1094b3bd20acc3badb7b489ce
Darryl Parks <djparks@usa.net>
2022-03-19 10:27:54 -0500

    Misc

--- src/main/java/net/parksy/j8/J8Application.java
+++ src/main/java/net/parksy/j8/J8Application.java
@@ -8,15 +8,19 @@ import picocli.CommandLine.Option;
 @Command(name = "j8", header = "%n@|green Jenerate Code Generator|@")
 public class J8Application implements Runnable {
 @CommandLine.Parameters(index = "0", description = "CSV Path/Filename", paramLabel = "CSV File") File csv;
-@CommandLine.Parameters(index = "1", description = "Template Path/Filename", paramLabel = "Template File") File template;
+//@CommandLine.Parameters(index = "1", description = "Template Path/Filename", paramLabel = "Template File") File template;
 
-    @Option(names = {"-f", "--file"}, required = false, description = "Filename")
+    @Option(names = {"-f", "--file"}, description = "Filename")
     private File file;
 
     @Override
     public void run() {
 //        System.out.printf("Loading %s%n", file.getAbsolutePath());
         System.out.printf("Loading %n");
+
+        System.out.println("Read Data Line by Line With Header \n");
+        ReadCSV.readDataLineByLine(csv.getAbsolutePath());
+        System.out.println("_______________________________________________");
     }
 
     public static void main(String[] args) {
